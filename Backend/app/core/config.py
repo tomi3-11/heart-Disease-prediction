88c6adac6d18b332d0d9ae562d2f25c31ef1ee0c
tom <didymusthomas6@gmail.com>
2026-08-01 20:10:04 +0300

    Feat: Backend restructure

---
+++ Backend/app/core/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+MODEL_PATH = BASE_DIR / "ml_artifacts" / "random_forest_model.joblib"
+ENCODER_PATH = BASE_DIR / "ml_artifacts" / "label_encoders.joblib"
+FEATURE_PATH = BASE_DIR / "ml_artifacts" / "feature_order.joblib"
+
+MODEL_VERSION = "1.0.0"
---
+++ Backend/app/main.py
@@ -0,0 +1,10 @@
+from fasfapi import FastAPI
+
+app = FastAPI(
+    title="Heart Disease Prediction API",
+    version="1.0.0",
+)
+
+@app.get("/")
+def root():
+    return {"message": "API is running"}

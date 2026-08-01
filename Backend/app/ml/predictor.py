2cd265003a7bde8348a3c0049b1c3800e73cd4a9
tom <didymusthomas6@gmail.com>
2026-08-01 23:54:01 +0300

    Add prediction of the model

---
+++ Backend/app/ml/predictor.py
@@ -0,0 +1,16 @@
+from app.ml.model_loader import get_model
+
+def predict(features):
+    """
+    Make prediction using the trained model.
+    """
+
+    model = get_model()
+
+    prediction = model.predict(features)[0]
+    probabilities = model.predict_proba(features)[0]
+
+    return {
+        "prediction": int(prediction),
+        "probability": float(probabilities[prediction]),
+    }

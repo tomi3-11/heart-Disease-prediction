a84b007f38151cbcc0caa075d05be94b09a6fb19
tom <didymusthomas6@gmail.com>
2026-08-02 13:12:47 +0300

    Feat: add database configs

---
+++ Backend/app/db/session.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)

0156a0c0965f1073284880445f50211367bfc2f7
tom <didymusthomas6@gmail.com>
2026-08-02 14:23:49 +0300

    Add patient crud functionalities

--- Backend/app/services/patient_service.py
+++ Backend/app/services/patient_service.py
@@ -22,3 +22,46 @@ def get_patients(db: Session):
 def get_patient(db: Session, patient_id: int):
     statement = select(Patient).where(Patient.id == patient_id)
     return db.scalar(statement)
+
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db.dependencies import get_db
+from app.schemas.patient import PatientCreate, PatientResponse
+from app.services.patient_service import (
+    create_patient,
+    get_patient,
+    get_patients,
+)
+
+router = APIRouter(
+    prefix="/patients",
+    tags=["Patients"],
+)
+
+
+@router.post("/", response_model=PatientResponse)
+def create(patient: PatientCreate, db: Session = Depends(get_db)):
+    return create_patient(db, patient)
+
+
+@router.get("/", response_model=list[PatientResponse])
+def read_all(db: Session = Depends(get_db)):
+    return get_patients(db)
+
+
+@router.get("/{patient_id}", response_model=PatientResponse)
+def read_one(patient_id: int, db: Session = Depends(get_db)):
+    patient = get_patient(db, patient_id)
+
+    if patient is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Patient not found"
+        )
+
+    return patient
+
+
+

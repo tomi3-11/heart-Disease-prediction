fe740e30022d6609415bbacff59454d8db893c9c
tom <didymusthomas6@gmail.com>
2026-08-02 14:22:06 +0300

    Add patient crud functionalities

--- Backend/app/routers/patients.py
+++ Backend/app/routers/patients.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.dependencies import get_db
 from app.schemas.patient import PatientCreate, PatientResponse
-from app.services.patient_service import create_patient
+from app.services.patient_service import (
+    create_patient,
+    get_patient,
+    get_patients,
+)
 
 router = APIRouter(
     prefix="/patients",
@@ -14,3 +18,21 @@ router = APIRouter(
 @router.post("/", response_model=PatientResponse)
 def create(patient: PatientCreate, db: Session = Depends(get_db)):
     return create_patient(db, patient)
+
+
+@router.get("/", response_model=list[PatientResponse])
+def read_all(db: Session = Depends(get_db)):
+    return get_patients(db)
+
+
+@router.get("/{patient_id}", response_model=PatientResponse)
+def read_one(patient_id: int, db: Session = Depends(get_db)):
+    patient = get_patient(db, patient_id)
+
+    if patient is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Patient not found"
+        )
+
+    return patient
--- Backend/app/services/patient_service.py
+++ Backend/app/services/patient_service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from app.models.patient import Patient
 from app.schemas.patient import PatientCreate
@@ -12,3 +13,12 @@ def create_patient(db: Session, patient: PatientCreate):
     db.refresh(db_patient)
 
     return db_patient
+
+def get_patients(db: Session):
+    statement = select(Patient)
+    return db.scalars(statement).all()
+
+
+def get_patient(db: Session, patient_id: int):
+    statement = select(Patient).where(Patient.id == patient_id)
+    return db.scalar(statement)

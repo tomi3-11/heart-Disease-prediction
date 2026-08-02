2d1c6223e2d4d0391dc4571197b9e58c6ce4fafe
tom <didymusthomas6@gmail.com>
2026-08-02 17:03:51 +0300

    Update mainentry point and requirements

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.ml.model_loader import load_artifacts
+from app.ml.explainer import load_explainer
 from app.routers.predictions import router as prediction_router
 from app.routers.patients import router as patient_router
 from app.routers.auth import router as auth_router
 from app.db.base import Base
 from app.db.session import engine
 import app.models
+
+from app.ml.preprocessing import preprocess
+from app.ml.explainer import explain
 
 
 app = FastAPI(
@@ -16,6 +20,7 @@ app = FastAPI(
 @app.on_event("startup")
 def startup():
     load_artifacts()
+    load_explainer()
   
 
 app.include_router(prediction_router)
@@ -26,7 +31,25 @@ app.include_router(auth_router)
 def root():
     return {"message": "API is running"}
 
-from app.core.security import (
-    hash_password,
-    verify_password,
-)
+
+@app.get("/test-shap")
+def test_shap():
+    sample = {
+        "Age": 40,
+        "Sex": "M",
+        "ChestPainType": "ATA",
+        "RestingBP": 120,
+        "Cholesterol": 250,
+        "FastingBS": 0,
+        "RestingECG": "Normal",
+        "MaxHR": 170,
+        "ExerciseAngina": "N",
+        "Oldpeak": 0.0,
+        "ST_Slope": "Up",
+    }
+
+    features = preprocess(sample)
+
+    return {
+        "shap_values": explain(features)
+    }
